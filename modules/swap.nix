e2dc2e2b09a94e6bf5fe62b221b3c8982201199a
Br3nnabee <contact@br3nnabee.dev>
2026-02-18 04:59:25 +0100

    Tweaked swap & added ramdisk

--- modules/swap.nix
+++ modules/swap.nix
@@ -6,7 +6,7 @@
     enable = true;
     algorithm = "zstd";
     memoryMax = 24 * 1024 * 1024 * 1024;
-    priority = 1000;
+    priority = 100;
     swapDevices = 1;
   };
 
@@ -19,16 +19,26 @@
     }
   ];
 
-  # === Low swappiness + zRAM-friendly VM tunables ===
+  # === High swappiness + zRAM-friendly VM tunables ===
   boot.kernel.sysctl = {
-    "vm.swappiness" = 10;
+    "vm.swappiness" = 100;
     # Prevents stalls, improves reclaim:
     "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
+    "vm.watermark_scale_factor" = 200;
     "vm.page-cluster" = 0;
+    "vm.dirty_ratio" = 60;
+    "vm.dirty_background_ratio" = 30;
   };
 
   # === Disable zswap ===
   # zswap + zRAM is redundant and can cause conflicts
   boot.kernelParams = [ "zswap.enabled=0" ];
+
+  # === RAMDisk ===
+  # Will be used for compilation primarily
+  fileSystems."/mnt/ramdisk" = {
+    device = "tmpfs";
+    fsType = "tmpfs";
+    options = [ "size = 32G" "mode = 1777" "nosuid" "nodev" ];
+  }
 }
